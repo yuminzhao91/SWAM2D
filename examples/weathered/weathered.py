ec9f2a4c5c7c90c6dbef223b24411c09cfabacc9
PageotD <damien.pageot@gmail.com>
2017-01-06 15:40:25 +0100

    Adding edge model and weathered model examples.

---
+++ examples/weathered/weathered.py
@@ -0,0 +1,34 @@
+#!/bin/python2.7
+
+import numpy as np
+
+n1 = 401
+n2 = 401
+
+vp = np.zeros((n1, n2), dtype=np.float32, order='F')
+vs = np.zeros((n1, n2), dtype=np.float32, order='F')
+ro = np.zeros((n1, n2), dtype=np.float32, order='F')
+
+vp[:, :] = 400.
+vp[200:401,:] = 600.
+
+vs[:, :] = 250.
+vs[200:401,:] = 375.
+
+ro[:, :] = 1500.
+ro[200:401,:] = 2250.
+
+fvp = open('fvp.bin', 'wb')
+vp2 = vp.swapaxes(0,1)
+vp2.tofile(fvp)
+fvp.close()
+
+fvs = open('fvs.bin', 'wb')
+vs2 = vs.swapaxes(0,1)
+vs2.tofile(fvs)
+fvs.close()
+
+fro = open('fro.bin', 'wb')
+ro2 = ro.swapaxes(0,1)
+ro2.tofile(fro)
+fro.close()
